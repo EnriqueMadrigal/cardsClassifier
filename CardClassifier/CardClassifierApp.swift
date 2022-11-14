1c056273b40606415e0ce2fa46faaab8bd34a7cd
EnriqueMadrigal <git@github.com>
2022-11-14 16:14:27 -0600

    First push

---
+++ CardClassifier/CardClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardClassifierApp.swift
+//  CardClassifier
+//
+//  Created by Algrthm on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CardClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
